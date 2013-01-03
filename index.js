785f90fac104957f1bda38459ce504ed0d65da6e
Forrest L Norvell <ogd@aoaioxxysz.net>
2013-01-03 01:45:36 -0800

    DRY and add support for async error handling.

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ var Migrations = require('./migrations');
 
 function nop() {}
 
+function migrate(operation, set, callback) {
+  if (!callback) callback = nop;
+
+  set.migrations.forEach(function (migration) {
+    set.resolve(migration, function (err) {
+      if (err) return callback(err);
+
+      migration[operation](callback);
+    });
+  });
+}
+
 module.exports = {
   Migrations : Migrations,
-  apply : function (set, callback) {
-    if (!callback) callback = nop;
-    set.migrations.forEach(function (migration) {
-      migration.apply(callback);
-    });
-  },
-  revert : function (set, callback) {
-    if (!callback) callback = nop;
-    set.migrations.forEach(function (migration) {
-      migration.revert(callback);
-    });
-  }
+  apply  : migrate.bind(null, 'apply'),
+  revert : migrate.bind(null, 'revert')
 };
